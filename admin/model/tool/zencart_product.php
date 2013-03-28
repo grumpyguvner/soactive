cf30de03a5fc502aa32ee3d8520d247f3d2006da
Tim Faulkner <tim@ants4t.co.uk>
2013-03-28 18:32:44 +0000

    Fix bug with zencart upload

--- admin/model/tool/zencart_product.php
+++ admin/model/tool/zencart_product.php
@@ -15,33 +15,33 @@ class ModelToolZencartProduct extends ModelToolZencart {
 //            $this->db->query("TRUNCATE " . DB_PREFIX . "attribute_group");
 //            $this->db->query("TRUNCATE " . DB_PREFIX . "attribute_group_description");
             
-            $this->db->query("TRUNCATE " . DB_PREFIX . "category");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "category_description");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "category_to_layout");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "category_to_store");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "category`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "category_description`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "category_to_layout`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "category_to_store`");
             
-            $this->db->query("TRUNCATE " . DB_PREFIX . "manufacturer");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "manufacturer_to_store");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "manufacturer`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "manufacturer_to_store`");
             
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_attribute");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_description");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_image");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_option");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_related");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_reward");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_special");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_to_category");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_to_download");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_to_layout");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "product_to_store");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_attribute`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_description`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_image`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_option`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_related`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_reward`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_special`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_to_category`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_to_download`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_to_layout`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "product_to_store`");
             
-            $this->db->query("TRUNCATE " . DB_PREFIX . "option");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "option_description");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "option_value");
-            $this->db->query("TRUNCATE " . DB_PREFIX . "option_value_description");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "option`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "option_description`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "option_value`");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "option_value_description`");
             
-            $this->db->query("TRUNCATE " . DB_PREFIX . "url_alias");
+            $this->db->query("TRUNCATE `" . DB_PREFIX . "url_alias`");
         }
 
         if (!$this->cacheZencartData())
