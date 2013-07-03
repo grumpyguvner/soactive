88d8295631e7c578baefb6c08e1426efade1ae76
antoniovillella <antoniovillella@antoniovillella-Lenovo-H520S>
2013-07-03 09:09:58 +0100

    #1339: Newsletter signup

---
+++ catalog/view/theme/soactive/language/english/module/newsletter.php
@@ -0,0 +1,28 @@
+<?php
+// Title
+$_['heading_title']         = 'Newsletter Subscription';
+
+// Entry
+$_['entry_name']            = 'Last Name';
+$_['entry_name2']           = 'First Name';
+$_['entry_email']           = 'Email Address';
+
+// Buttons
+$_['button_go'] 	    = 'Subscribe';
+
+// Text
+$_['text_subscribed'] 	    = "'%s' has been added to our mailing list.";
+$_['text_subscribed_optin'] = "'%s' has been added, please check your inbox to confirm this subscription.";
+$_['text_unsubscribed']     = "'%s' has been removed from our mailing list.";
+$_['text_signup'] 	        = 'Signup for our newsletter!';
+$_['text_subscribe']     	= 'Subscribe';
+$_['text_unsubscribe']      = 'Unsubscribe';
+
+// Error
+$_['error_exists'] 		= "'%s' has been already been added to our mailing list.";
+$_['error_not_found']           = 'Email Not Found!';
+$_['error_email'] 		= "'%s' is not a valid email address.";
+$_['error_name'] 		= 'Invalid Name!';
+$_['error_firstname']           = 'Firstname is required!';
+$_['error_lastname']            = 'Lastname is required!';
+?>
