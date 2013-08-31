8392ff0ce1db52dba71cadcadf047b8075b2a00d
Tim Faulkner <tim@ants4t.co.uk>
2013-08-31 11:34:41 +0100

    Fix bug with product model not caching

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -935,7 +935,6 @@ class ModelCatalogProduct extends Model {
         $cache = md5(http_build_query($data));
 
         $product_data = $this->cache->get('product.total.' . (int) $this->config->get('config_language_id') . '.' . (int) $this->config->get('config_store_id') . '.' . (int) $customer_group_id . '.' . $cache);
-        $product_data = false;
 
         if (!$product_data) {
             $myCategories = array();
