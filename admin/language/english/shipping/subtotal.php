40edc2a54f088a84d52800e7350c91a428e2200a
Tim Faulkner <tim@ants4t.co.uk>
2013-09-03 18:25:21 +0100

    #1644 Added subtotal shipping

---
+++ admin/language/english/shipping/subtotal.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Subtotal Based Shipping';
+
+// Text
+$_['text_shipping']    = 'Shipping';
+$_['text_success']     = 'Success: You have modified subtotal based shipping!';
+
+// Entry
+$_['entry_rate']       = 'Rates:<br /><span class="help">Example: 5.00:10.00,7.00:12.00 Subtotal:Cost,Subtotal:Cost, etc..</span>';
+$_['entry_tax_class']  = 'Tax Class:';
+$_['entry_geo_zone']   = 'Geo Zone:';
+$_['entry_status']     = 'Status:';
+$_['entry_sort_order'] = 'Sort Order:';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify subtotal based shipping!';
+?>
