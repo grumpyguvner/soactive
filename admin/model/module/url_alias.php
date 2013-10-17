20518e44c88c7f3aa1308bbc607db3e06ef70b79
Tim Faulkner <tim@ants4t.co.uk>
2013-10-17 15:31:08 +0100

    Extend url_alias to keep keyword history

--- admin/model/module/url_alias.php
+++ admin/model/module/url_alias.php
@@ -47,7 +47,7 @@ class ModelModuleUrlAlias extends Model {
             //Unwanted:  {UPPERCASE} ; / ? : @ & = + $ , . ! ~ * ' ( )
             $string = strtolower($string);
             //Strip any unwanted characters
-            $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+            $string = preg_replace("/[^a-z0-9_\s-\.]/", "", $string);
             //Clean multiple dashes or whitespaces
             $string = preg_replace("/[\s-]+/", " ", $string);
             //Convert whitespaces and underscore to dash
