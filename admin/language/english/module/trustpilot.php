f6686f0ddc6adf3a1b5ec8a1fd0e3e6cce6f8e8a
matty.hill-andrews <matty.hill-andrews@markhorton-Lenovo-H520S>
2013-10-29 14:50:40 +0000

    Added trust pilot module

---
+++ admin/language/english/module/trustpilot.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']       = 'Trust Pilot';
+
+// Text
+$_['text_success']        = 'Success: You have modified module postcode anywhere!';
+
+// Entry
+$_['entry_status']        = 'Status:';
+$_['entry_email']         = 'Email:';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module trust pilot!';
+?>
