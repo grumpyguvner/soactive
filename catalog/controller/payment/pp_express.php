89a31d827c71768ff43859f8aa893c98c850a7ce
Mark <Mark@mark-lenovo.home>
2014-02-18 10:39:03 +0000

    Paypal Error on soactive.fr caused by rounding on item total, bug fix on currency calculations.

--- catalog/controller/payment/pp_express.php
+++ catalog/controller/payment/pp_express.php
@@ -83,8 +83,8 @@ class ControllerPaymentPPExpress extends Controller {
             //       $itemTotalValue + $taxTotalValue;
 
             $paymentDetails->ShipToAddress = $address;
-            $paymentDetails->ItemTotal = new BasicAmountType($currencyCode, $this->currency->format($itemTotalValue, $order_info['currency_code'], false, false));
-            $paymentDetails->OrderTotal = new BasicAmountType($currencyCode, $this->currency->format($order_info['total'], $order_info['currency_code'], false, false));
+            $paymentDetails->ItemTotal = new BasicAmountType($currencyCode, $this->currency->format($itemTotalValue, $order_info['currency_code'], 1, false));
+            $paymentDetails->OrderTotal = new BasicAmountType($currencyCode, $this->currency->format($order_info['total'], $order_info['currency_code'], 1, false));
             //$paymentDetails->TaxTotal = new BasicAmountType($currencyCode, $taxTotalValue);
 
             if ($this->config->get('pp_express_method')) {
@@ -95,7 +95,7 @@ class ControllerPaymentPPExpress extends Controller {
 
 //        $paymentDetails->HandlingTotal = $handlingTotal;
 //        $paymentDetails->InsuranceTotal = $insuranceTotal;
-            $paymentDetails->ShippingTotal = new BasicAmountType($currencyCode, $this->currency->format($order_info['total'] - $itemTotalValue, $order_info['currency_code'], false, false));
+            $paymentDetails->ShippingTotal = new BasicAmountType($currencyCode, $this->currency->format($order_info['total'], $order_info['currency_code'], 1, false)-$this->currency->format($itemTotalValue, $order_info['currency_code'], 1, false));
 //        if(isset($_REQUEST['notifyURL']))
 //        {
 //                $paymentDetails->NotifyURL = $_REQUEST['notifyURL'];
